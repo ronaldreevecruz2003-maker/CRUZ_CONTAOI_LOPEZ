8b57915dbadbad5c392df90c4d693f694485410b
Ronald Reeve Cruz <ronaldreevecruz2003@gmail.com>
2026-08-17 20:40:55 +0800

    Phase 2

--- app/src/main/java/com/example/unscrambletropa/MainActivity.kt
+++ app/src/main/java/com/example/unscrambletropa/MainActivity.kt
@@ -9,12 +9,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.unscrambletropa.ui.theme.UnscrambleTropaTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,31 +29,58 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnscrambleTropaTheme {
-                Gamescreen()
+                GameScreen()
             }
         }
     }
 }
 
-
 @Composable
-    fun Gamescreen(){
-    Column(modifier = Modifier.fillMaxSize(),
-           horizontalAlignment = Alignment.CenterHorizontally,
-           verticalArrangement = Arrangement.Center) {
-        Text("UNSCRAMBLE")
-        Text("TAC")
-        Text("Unscramble the word!")
-        OutlinedTextField(
-            value = userGuess,
-            onValueChange = {  },
-            label = { Text("Enter your answer") }
+fun GameScreen() {
+    var userAnswer by remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "UNSCRAMBLE",
+            fontSize = 30.sp
         )
-        Button(onClick = {}) {
+        Text(
+            text = "TAC",
+            fontSize = 40.sp
+        )
+        Text(
+            text = "Unscramble the word!"
+        )
+        OutlinedTextField(
+            value = userAnswer,
+            onValueChange = {
+                userAnswer = it
+            },
+            label = {
+                Text("Enter your answer")
+            }
+        )
+
+        Button(
+            onClick = { }
+        ) {
             Text("SUBMIT")
         }
-
-        Text( "Score:0")
+        Text(
+            text = "Score: 0"
+        )
     }
-    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun GameScreenPreview() {
+    UnscrambleTropaTheme {
+        GameScreen()
+    }
+}
